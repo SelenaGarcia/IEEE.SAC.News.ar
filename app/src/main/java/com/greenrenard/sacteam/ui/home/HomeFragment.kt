c7a38700914e13b222d250b69640423f592155e5
Selena Garcia <selenagarcialobo@ieee.org>
2020-06-13 21:36:00 -0300

    background n sac image

--- app/src/main/java/com/greenrenard/sacteam/ui/home/HomeFragment.kt
+++ app/src/main/java/com/greenrenard/sacteam/ui/home/HomeFragment.kt
@@ -8,11 +8,14 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.bumptech.glide.Glide
 import com.greenrenard.sacteam.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    private val sacImagen = "https://firebasestorage.googleapis.com/v0/b/wescis2018.appspot.com/o/logo_SAC.png?alt=media&token=25421368-fbea-4752-b68b-03e9d2afa547"
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -22,10 +25,19 @@ class HomeFragment : Fragment() {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
+        val textView: TextView = root.findViewById(R.id.textoInicio)
+
+
+
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+          //  textView.text = textoInicio.text
+
+            Glide.with(this).load(sacImagen).into(sacImagenId)
+
         })
         return root
     }
+
+
+
 }
